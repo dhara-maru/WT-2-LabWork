d47f9e4cec8f75c89a01a529c2a76eae8cd3f4d5
dhara-maru <dharamaru406@gmail.com>
2025-03-19 10:47:28 +0530

    19 march

--- Angular/cwh/src/app/components/home-component/home-component.component.ts
+++ Angular/cwh/src/app/components/home-component/home-component.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
 export class HomeComponentComponent {
+  active = true;
+  inActive = false;
+  myColor = 'blue';
 
+  togglefun(){
+    this.active = !this.active;
+    this.inActive = !this.inActive;
+  }
 }
