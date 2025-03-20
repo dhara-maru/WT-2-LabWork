5bf56467190fbdb004df2087613249d60b9a7aa4
dhara-maru <dharamaru406@gmail.com>
2025-03-20 11:27:10 +0530

    facultis add, edit , delete

--- Angular/cwh/src/app/components/faculty/faculty.component.ts
+++ Angular/cwh/src/app/components/faculty/faculty.component.ts
@@ -11,12 +11,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class FacultyComponent {
   faculty = { 
-    id: "", 
+    id: 0, 
     name: "", 
     designation: "", 
     degree: "", 
     experience: "", 
-    imageURL: "" 
+    imgURL: "" 
   };
 
   submitted = false;
@@ -25,8 +25,74 @@ export class FacultyComponent {
     if (form.valid) {
       this.submitted = true;
       console.log('Faculty Data:', this.faculty); 
-      this.faculty = { id: "", name: "", designation: "", degree: "", experience: "", imageURL: "" };
+
+      if(this.faculty.id ==0){
+        this.faculties = [...this.faculties,
+           {...this.faculty, id: this.faculties.length > 0 ? this.faculties[this.faculties.length-1].id + 1 : 1 }
+          ]
+      }
+      else{
+        this.faculties = this.faculties.map((fac) => fac.id == this.faculty.id ? this.faculty : fac)
+      }
+
+
+
+
+
+      this.faculty = { id: 0, name: "", designation: "", degree: "", experience: "", imgURL: "" };
       form.resetForm();
     }
+  }
+
+
+ faculties = [
+  {
+    id : 1, 
+    name : "Dr. Gopi Sanghani", 
+    designation : "Dean - Computer Science", 
+    degree : "Ph.D. , M.E. (CE)", 
+    experience : "24+ Years", 
+    imgURL : "https://du-website.s3.ap-south-1.amazonaws.com/U01/Faculty-Photo/5---29-04-2023-11-00-29.jpg"
+  },
+  {
+    id : 2, 
+    name : "Dr. Nilesh Gambhava", 
+    designation : "Professor", 
+    degree : "Ph.D. , M.E. (CE)", 
+    experience : "22+ Years", 
+    imgURL : "https://du-website.s3.ap-south-1.amazonaws.com/U01/Faculty-Photo/3---28-04-2023-02-02-42.jpg"
+  },
+  {
+    id : 3, 
+    name : "Dr. Pradyumansinh Jadeja", 
+    designation : "Professor", 
+    degree : "Ph.D. , M.E. (CE)", 
+    experience : "19+ Years", 
+    imgURL : "https://du-website.s3.ap-south-1.amazonaws.com/U01/Faculty-Photo/6---28-04-2023-02-06-07.jpg"
+  },
+  {
+    id : 4, 
+    name : "Prof. Maulik Trivedi", 
+    designation : "Professor", 
+    degree : "Ph.D. , M.E. (CE)", 
+    experience : "12+ Years", 
+    imgURL : "https://du-website.s3.ap-south-1.amazonaws.com/U01/Faculty-Photo/8---28-04-2023-02-06-25.jpg"
+  },
+  {
+    id : 5, 
+    name : "Prof. Firoz Sherasiya", 
+    designation : "Professor", 
+    degree : "Ph.D.(Pursuing), M.E. (CE)", 
+    experience : "20+ Years", 
+    imgURL : "https://du-website.s3.ap-south-1.amazonaws.com/U01/Faculty-Photo/12---28-04-2023-02-06-51.jpg"
+  },
+  ]
+
+  onDelete(id : any){
+    this.faculties = this.faculties.filter((fac) => fac.id != id)
+  }
+
+  onEdit(fac : any){
+    this.faculty = {...fac}
   }
 }
